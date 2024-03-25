c5c5e9099f719587cfc3e1208f94633faad5f4c0
AdryannDev <160923615+AdryannDev@users.noreply.github.com>
2024-03-25 20:56:41 -0300

    Update ExerciciosPartePratica02.py

--- Aula06/ExerciciosPartePratica02.py
+++ Aula06/ExerciciosPartePratica02.py
@@ -3,7 +3,7 @@ a = int(input('Insira o valor: '))
 if a < 1001:
     b = a * 0.1
     print('o desconto é:', b)
-    print('valor total é:', a-b)
+    print('valor total é:', a - b)
 elif 1000 < a < 5000:
     b = a * 0.2
     print('o desconto é:', b)
